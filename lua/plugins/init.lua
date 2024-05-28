70fec595021c7dfbcae4aabbe288a187c73af9bb
livghit <liviu.g01@gmail.com>
2024-05-28 20:24:03 +0200

    wip

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,50 +1,50 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    config = function()
-      require "configs.conform"
-    end,
-  },
+	{
+		"stevearc/conform.nvim",
+		-- event = 'BufWritePre', -- uncomment for format on save
+		config = function()
+			require "configs.conform"
+		end,
+	},
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("nvchad.configs.lspconfig").defaults()
-      require "configs.lspconfig"
-    end,
-  },
+	-- These are some examples, uncomment them if you want to see them work!
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("nvchad.configs.lspconfig").defaults()
+			require "configs.lspconfig"
+		end,
+	},
 
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "tailwindcss-language-server",
-        "intelephense",
-        "typescript-language-server",
-        "vue-language-server",
-        "lua-language-server",
-        "pint",
-        "stylua",
-        "html-lsp",
-        "css-lsp",
-        "prettier",
-      },
-    },
-  },
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"tailwindcss-language-server",
+				"intelephense",
+				"typescript-language-server",
+				"vue-language-server",
+				"lua-language-server",
+				"pint",
+				"stylua",
+				"html-lsp",
+				"css-lsp",
+				"prettier",
+			},
+		},
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "vim",
-        "lua",
-        "php",
-        "html",
-        "css",
-        "typescript",
-      },
-    },
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"vim",
+				"lua",
+				"php",
+				"html",
+				"css",
+				"typescript",
+			},
+		},
+	},
 }
