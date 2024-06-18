9423c505212c8470144b56d8075500be0fb84e18
livghit <liviu.g01@gmail.com>
2024-06-18 16:28:39 +0200

    wip

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,20 +1,22 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-     css = { "prettier" },
-     html = { "prettier" },
-     javascript = { "prettier" },
-     javascriptreact = { "prettier" },
-     vue = { "prettier" },
-     php = { "pint" },
-     ruby = { "rubyfmt" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		css = { "prettier" },
+		html = { "prettier" },
+		javascript = { "prettier" },
+		javascriptreact = { "prettier" },
+		vue = { "prettier" },
+		php = { "pint" },
+		ruby = { "rubyfmt" },
+    -- not working needs fix  |>
+		erb = { "erb-formatter" },
+	},
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+	-- format_on_save = {
+	--   -- These options will be passed to conform.format()
+	--   timeout_ms = 500,
+	--   lsp_fallback = true,
+	-- },
 }
 
 require("conform").setup(options)
