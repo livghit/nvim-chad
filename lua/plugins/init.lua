e3b4796cfbec02d52144f5f9d55875f6de63cc56
livghit <liviu.g01@gmail.com>
2024-06-20 08:04:57 +0200

    wip

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -36,11 +36,31 @@ return {
 
 	{
 		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function(_, opts)
+			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+			parser_config.blade = {
+				install_info = {
+					url = "https://github.com/EmranMR/tree-sitter-blade",
+					files = {
+						"src/parser.c",
+					},
+					branch = "main",
+					generate_requires_npm = true,
+					requires_generate_from_grammar = true,
+				},
+				filetype = "blade",
+			}
+
+			require("nvim-treesitter.configs").setup(opts)
+		end,
 		opts = {
 			ensure_installed = {
 				"vim",
 				"lua",
 				"php",
+				"php_only",
 				"html",
 				"css",
 				"typescript",
